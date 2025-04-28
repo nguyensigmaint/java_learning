7ba5fe53046cee588b45b9149bd6ea0829c9be9c
グェン　カン　フン <khanhung913@gmail.com>
2025-04-28 17:56:01 +0900

    0428

--- janken_nguyenkhanhhung/src/excellence/Player.java
+++ janken_nguyenkhanhhung/src/excellence/Player.java
@@ -8,13 +8,15 @@ import java.util.Random;
 public class Player {
 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	Random rd = new Random();
+
 	public Player() {
-		
+
 	}
+
 	public int playerHandInput() {
 		int playerChoice = 0;
 		do {
-			String tempPlayerChoice = null;
+			String tempPlayerChoice;
 			try {
 				tempPlayerChoice = br.readLine();
 				playerChoice = Integer.parseInt(tempPlayerChoice);
@@ -28,8 +30,52 @@ public class Player {
 		} while (playerChoice < 0 || playerChoice > 2);
 		return playerChoice;
 	}
-	
-	public int computerChoiceHand() {
-		return rd.nextInt(3);
+
+	public int computerChoiceHand(int[][] history, int i) {
+		int computerChoiceHand;
+		int randomStrategy = rd.nextInt(3);
+		//プレイヤーの手を真似する
+		if (randomStrategy == 0) {
+			if (i > 0) {
+				computerChoiceHand = history[i - 1][0];
+			} else {
+				computerChoiceHand = rd.nextInt(3);
+			}
+		}
+		//同じ手を出さない
+		else if (randomStrategy == 1) {
+			if (i > 0) {
+				do {
+					computerChoiceHand = rd.nextInt(3);
+				} while (computerChoiceHand == history[i - 1][1]);
+			} else {
+				computerChoiceHand = rd.nextInt(3);
+			}
+		} 
+		//random
+		else {
+			computerChoiceHand = rd.nextInt(3);
+		}
+		return computerChoiceHand;
 	}
+
+	public int choiceNumbersOfJanken() {
+		int numbersOfJanken = 0;
+		do {
+			String tempNumbersOfChoice;
+			try {
+				tempNumbersOfChoice = br.readLine();
+				numbersOfJanken = Integer.parseInt(tempNumbersOfChoice);
+			} catch (NumberFormatException | IOException c) {
+				System.out.println("システムエラーが発生しました。");
+				System.out.println("システムが終了です。");
+				System.exit(0);
+			}
+			if (numbersOfJanken < 1) {
+				System.out.println("回数をもう一度入力してください");
+			}
+		} while (numbersOfJanken < 1);
+		return numbersOfJanken;
+	}
+
 }
--- janken_nguyenkhanhhung/src/excellence/SystemMain.java
+++ janken_nguyenkhanhhung/src/excellence/SystemMain.java
@@ -1,23 +1,25 @@
 package excellence;
 
-
 public class SystemMain {
 
 	public static void main(String[] args) {
 		System.out.println("【じゃんけん開始】");
-		JankenJudge jankenJudge= new JankenJudge();
-		Player player=new Player();
-		
-		for (int i = 1; i <= 3; i++) {
-			System.out.println(i + "【回戦目】");
+		JankenJudge jankenJudge = new JankenJudge();
+		Player player = new Player();
+		System.out.println("じゃんけんの回数を入力してください");
+		int jankenOfNumbers = player.choiceNumbersOfJanken();
+		int jankenHistory[][] = new int[jankenOfNumbers][2];
+
+		for (int i = 1; i <= jankenOfNumbers; i++) {
+			System.out.println("【" + i + "回戦目】");
 			System.out.println("プレイヤーの手を決めてください");
 			System.out.println("(グー:0、チョキ:1、パー:2)");
-			int computerChoice = player.computerChoiceHand();
-			int playerChoice=player.playerHandInput();
-			jankenJudge.checkJanken(playerChoice, computerChoice);
+			jankenJudge.checkJanken(player.computerChoiceHand(jankenHistory,i), player.playerHandInput());
+			jankenHistory[i][0]=player.playerHandInput();
+			jankenHistory[i][1]=player.computerChoiceHand(jankenHistory,i);
 		}
 		System.out.println("【じゃんけん終了】");
 		jankenJudge.checkAllResult();
-		
+
 	}
 }
